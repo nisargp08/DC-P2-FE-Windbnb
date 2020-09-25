4baedf133edef04da111dc1377b41aae8f961a0c
Nisarg Patel <nisargp42@gmail.com>
2020-09-25 00:32:02 -0400

    Mobile :: Landing :: Inprogress

--- src/App.vue
+++ src/App.vue
@@ -1,20 +1,223 @@
 <template>
-  <div id="app">
-  </div>
+<div id="app">
+    <div class="min-h-screen">
+        <!-- Logo -->
+        <div class="pt-4 pl-3">
+            <svg width="97" height="26" viewBox="0 0 97 26" fill="none" xmlns="http://www.w3.org/2000/svg">
+                <path d="M40.082 13.188L37.968 21H35.322L34.09 15.932L32.816 21H30.184L28.056 13.188H30.45L31.556 18.774L32.872 13.188H35.406L36.736 18.746L37.828 13.188H40.082ZM42.199 12.376C41.779 12.376 41.4336 12.2547 41.163 12.012C40.9016 11.76 40.771 11.452 40.771 11.088C40.771 10.7147 40.9016 10.4067 41.163 10.164C41.4336 9.912 41.779 9.786 42.199 9.786C42.6096 9.786 42.9456 9.912 43.207 10.164C43.4776 10.4067 43.613 10.7147 43.613 11.088C43.613 11.452 43.4776 11.76 43.207 12.012C42.9456 12.2547 42.6096 12.376 42.199 12.376ZM43.389 13.188V21H40.995V13.188H43.389ZM49.8839 13.104C50.7985 13.104 51.5265 13.4027 52.0679 14C52.6185 14.588 52.8939 15.4 52.8939 16.436V21H50.5139V16.758C50.5139 16.2353 50.3785 15.8293 50.1079 15.54C49.8372 15.2507 49.4732 15.106 49.0159 15.106C48.5585 15.106 48.1945 15.2507 47.9239 15.54C47.6532 15.8293 47.5179 16.2353 47.5179 16.758V21H45.1239V13.188H47.5179V14.224C47.7605 13.8787 48.0872 13.608 48.4979 13.412C48.9085 13.2067 49.3705 13.104 49.8839 13.104ZM54.0815 17.08C54.0815 16.2773 54.2308 15.5727 54.5295 14.966C54.8375 14.3593 55.2528 13.8927 55.7755 13.566C56.2981 13.2393 56.8815 13.076 57.5255 13.076C58.0388 13.076 58.5055 13.1833 58.9255 13.398C59.3548 13.6127 59.6908 13.902 59.9335 14.266V10.64H62.3275V21H59.9335V19.88C59.7095 20.2533 59.3875 20.552 58.9675 20.776C58.5568 21 58.0761 21.112 57.5255 21.112C56.8815 21.112 56.2981 20.9487 55.7755 20.622C55.2528 20.286 54.8375 19.8147 54.5295 19.208C54.2308 18.592 54.0815 17.8827 54.0815 17.08ZM59.9335 17.094C59.9335 16.4967 59.7655 16.0253 59.4295 15.68C59.1028 15.3347 58.7015 15.162 58.2255 15.162C57.7495 15.162 57.3435 15.3347 57.0075 15.68C56.6808 16.016 56.5175 16.4827 56.5175 17.08C56.5175 17.6773 56.6808 18.1533 57.0075 18.508C57.3435 18.8533 57.7495 19.026 58.2255 19.026C58.7015 19.026 59.1028 18.8533 59.4295 18.508C59.7655 18.1627 59.9335 17.6913 59.9335 17.094ZM66.4534 14.294C66.6774 13.93 66.9994 13.636 67.4194 13.412C67.8394 13.188 68.3201 13.076 68.8614 13.076C69.5054 13.076 70.0887 13.2393 70.6114 13.566C71.1341 13.8927 71.5447 14.3593 71.8434 14.966C72.1514 15.5727 72.3054 16.2773 72.3054 17.08C72.3054 17.8827 72.1514 18.592 71.8434 19.208C71.5447 19.8147 71.1341 20.286 70.6114 20.622C70.0887 20.9487 69.5054 21.112 68.8614 21.112C68.3107 21.112 67.8301 21.0047 67.4194 20.79C67.0087 20.566 66.6867 20.272 66.4534 19.908V21H64.0594V10.64H66.4534V14.294ZM69.8694 17.08C69.8694 16.4827 69.7014 16.016 69.3654 15.68C69.0387 15.3347 68.6327 15.162 68.1474 15.162C67.6714 15.162 67.2654 15.3347 66.9294 15.68C66.6027 16.0253 66.4394 16.4967 66.4394 17.094C66.4394 17.6913 66.6027 18.1627 66.9294 18.508C67.2654 18.8533 67.6714 19.026 68.1474 19.026C68.6234 19.026 69.0294 18.8533 69.3654 18.508C69.7014 18.1533 69.8694 17.6773 69.8694 17.08ZM78.3214 13.104C79.236 13.104 79.964 13.4027 80.5054 14C81.056 14.588 81.3314 15.4 81.3314 16.436V21H78.9514V16.758C78.9514 16.2353 78.816 15.8293 78.5454 15.54C78.2747 15.2507 77.9107 15.106 77.4534 15.106C76.996 15.106 76.632 15.2507 76.3614 15.54C76.0907 15.8293 75.9554 16.2353 75.9554 16.758V21H73.5614V13.188H75.9554V14.224C76.198 13.8787 76.5247 13.608 76.9354 13.412C77.346 13.2067 77.808 13.104 78.3214 13.104ZM85.389 14.294C85.613 13.93 85.935 13.636 86.355 13.412C86.775 13.188 87.2556 13.076 87.797 13.076C88.441 13.076 89.0243 13.2393 89.547 13.566C90.0696 13.8927 90.4803 14.3593 90.779 14.966C91.087 15.5727 91.241 16.2773 91.241 17.08C91.241 17.8827 91.087 18.592 90.779 19.208C90.4803 19.8147 90.0696 20.286 89.547 20.622C89.0243 20.9487 88.441 21.112 87.797 21.112C87.2463 21.112 86.7656 21.0047 86.355 20.79C85.9443 20.566 85.6223 20.272 85.389 19.908V21H82.995V10.64H85.389V14.294ZM88.805 17.08C88.805 16.4827 88.637 16.016 88.301 15.68C87.9743 15.3347 87.5683 15.162 87.083 15.162C86.607 15.162 86.201 15.3347 85.865 15.68C85.5383 16.0253 85.375 16.4967 85.375 17.094C85.375 17.6913 85.5383 18.1627 85.865 18.508C86.201 18.8533 86.607 19.026 87.083 19.026C87.559 19.026 87.965 18.8533 88.301 18.508C88.637 18.1533 88.805 17.6773 88.805 17.08Z" fill="#EB5757" fill-opacity="0.8" />
+                <path d="M9.22724 7.5C10.3819 5.5 13.2687 5.5 14.4234 7.5L20.0526 17.25C21.2073 19.25 19.7639 21.75 17.4545 21.75H6.19615C3.88675 21.75 2.44338 19.25 3.59808 17.25L9.22724 7.5Z" fill="#EB5757" fill-opacity="0.8" />
+            </svg>
+        </div>
+        <!-- Search Box -->
+        <div class="mt-10">
+            <div class="flex items-center justify-center mx-auto w-90 rounded-16px bg-white shadow-white ">
+                <!-- Location -->
+                <input class="w-33 border-r border-off-white p-4 font-mulish font-normal leading-4 text-sm text-black" type="text" value="Helsinki,Finland">
+                <!-- Guests -->
+                <input class="w-32 border-r border-off-white p-4 font-mulish font-normal leading-4 text-sm text-gray-1" type="text" placeholder="Add guests">
+                <!-- Search button -->
+                <button class="p-4">
+                    <svg class="w-6 h-6 fill-current text-primary" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg">
+                        <path fill-rule="evenodd" d="M8 4a4 4 0 100 8 4 4 0 000-8zM2 8a6 6 0 1110.89 3.476l4.817 4.817a1 1 0 01-1.414 1.414l-4.816-4.816A6 6 0 012 8z" clip-rule="evenodd" /></svg>
+                </button>
+            </div>
+        </div>
+
+        <!-- Property Info Heading -->
+        <div class="flex items-center justify-between mt-8 px-3">
+            <h3 class="font-montserrat font-bold text-lg text-black leading-5">Stays in Finland</h3>
+            <p class="font-montserrat font-medium text-sm leading-4 text-secondary">12+ stays</p>
+        </div>
+        <!-- Properties -->
+        <div v-for="(property,index) in properties" :key="index" class="my-6 px-3">
+            <img class="rounded-24px" :src="property.photo" :alt="property.title">
+            <div class="flex items-center justify-between pt-4">
+                <div class="flex items-center gap-2">
+                    <p v-if="property.superHost" class="font-montserrat font-bold text-10px leading-3 py-2 px-3 border border-secondary rounded-12px uppercase">super host</p>
+                    <p class="font-montserrat font-medium text-xs leading-4 text-tertiary">{{ property.type }} <span class="dot">.</span> {{ property.beds}} beds </p>
+                </div>
+                <div class="flex items-center">
+                    <svg class="w-4 h-4 fill-current text-primary-opaque" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg">
+                        <path d="M9.049 2.927c.3-.921 1.603-.921 1.902 0l1.07 3.292a1 1 0 00.95.69h3.462c.969 0 1.371 1.24.588 1.81l-2.8 2.034a1 1 0 00-.364 1.118l1.07 3.292c.3.921-.755 1.688-1.54 1.118l-2.8-2.034a1 1 0 00-1.175 0l-2.8 2.034c-.784.57-1.838-.197-1.539-1.118l1.07-3.292a1 1 0 00-.364-1.118L2.98 8.72c-.783-.57-.38-1.81.588-1.81h3.461a1 1 0 00.951-.69l1.07-3.292z"></path>
+                    </svg>
+                    <p class="font-montserrat font-medium text-xs leading-4 text-secondary">{{ property.rating }}</p>
+                </div>
+            </div>
+            <p class="font-montserrat font-medium text-xs leading-4 text-black pt-2">{{ property.title }}</p>
+        </div>
+    </div>
+</div>
 </template>
 
 <script>
-
 export default {
-  name: 'App',
-  components: {
-  }
+    name: 'App',
+    data() {
+        return {
+            properties: [{
+                    "city": "Helsinki",
+                    "country": "Finland",
+                    "superHost": false,
+                    "title": "Stylist apartment in center of the city",
+                    "rating": 4.4,
+                    "maxGuests": 3,
+                    "type": "Entire apartment",
+                    "beds": 2,
+                    "photo": "https://images.unsplash.com/photo-1505873242700-f289a29e1e0f?ixlib=rb-1.2.1&auto=format&fit=crop&w=2255&q=80"
+                },
+                {
+                    "city": "Turku",
+                    "country": "Finland",
+                    "superHost": false,
+                    "title": "Nice apartment in center of Helsinki",
+                    "rating": 4.2,
+                    "maxGuests": 5,
+                    "type": "Entire apartment",
+                    "beds": 3,
+                    "photo": "https://images.unsplash.com/photo-1554995207-c18c203602cb?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=2250&q=80"
+                },
+                {
+                    "city": "Helsinki",
+                    "country": "Finland",
+                    "superHost": true,
+                    "title": "Arty interior in 1900 wooden house",
+                    "rating": 4.5,
+                    "maxGuests": 10,
+                    "type": "Entire house",
+                    "beds": 6,
+                    "photo": "https://images.unsplash.com/photo-1505691938895-1758d7feb511?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=2250&q=80"
+                },
+                {
+                    "city": "Helsinki",
+                    "country": "Finland",
+                    "superHost": false,
+                    "title": "Apartment next to market spuare",
+                    "rating": 4.48,
+                    "maxGuests": 3,
+                    "type": "Entire apartment",
+                    "beds": null,
+                    "photo": "https://images.unsplash.com/photo-1556020685-ae41abfc9365?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=934&q=80"
+                },
+                {
+                    "city": "Turku",
+                    "country": "Finland",
+                    "superHost": true,
+                    "title": "Villa Aurora guest-house",
+                    "rating": 4.75,
+                    "maxGuests": 9,
+                    "type": "Entire house",
+                    "beds": null,
+                    "photo": "https://images.unsplash.com/photo-1513694203232-719a280e022f?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=2249&q=80"
+                },
+                {
+                    "city": "Vaasa",
+                    "country": "Finland",
+                    "superHost": true,
+                    "title": "A cosy family house",
+                    "rating": 4.95,
+                    "maxGuests": 6,
+                    "type": "Entire house",
+                    "beds": null,
+                    "photo": "https://images.unsplash.com/photo-1524758631624-e2822e304c36?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=2250&q=80"
+                },
+                {
+                    "city": "Vaasa",
+                    "country": "Finland",
+                    "superHost": false,
+                    "title": "Lovely Studio near Railway Station",
+                    "rating": 4.68,
+                    "maxGuests": 2,
+                    "type": "Private room",
+                    "beds": null,
+                    "photo": "https://images.unsplash.com/photo-1505693314120-0d443867891c?ixlib=rb-1.2.1&auto=format&fit=crop&w=2591&q=80"
+                },
+                {
+                    "city": "Oulu",
+                    "country": "Finland",
+                    "superHost": false,
+                    "title": "Peaceful little home in city center",
+                    "rating": 4.12,
+                    "maxGuests": 6,
+                    "type": "Entire house",
+                    "beds": 3,
+                    "photo": "https://images.unsplash.com/photo-1522708323590-d24dbb6b0267?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=2250&q=80"
+                },
+                {
+                    "city": "Oulu",
+                    "country": "Finland",
+                    "superHost": false,
+                    "title": "Beautiful new studio apartment nearby the center",
+                    "rating": 4.49,
+                    "maxGuests": 2,
+                    "type": "Entire apartment",
+                    "beds": 1,
+                    "photo": "https://images.unsplash.com/photo-1507089947368-19c1da9775ae?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=2255&q=80"
+                },
+                {
+                    "city": "Oulu",
+                    "country": "Finland",
+                    "superHost": true,
+                    "title": "Cozy woodhouse flat with wooden sauna",
+                    "rating": 4.38,
+                    "maxGuests": 4,
+                    "type": "Entire house",
+                    "beds": null,
+                    "photo": "https://images.unsplash.com/photo-1522156373667-4c7234bbd804?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjF9&auto=format&fit=crop&w=930&q=80"
+                },
+                {
+                    "city": "Vaasa",
+                    "country": "Finland",
+                    "superHost": false,
+                    "title": "Brand new studio apartment near the harbour",
+                    "rating": 4.89,
+                    "maxGuests": 6,
+                    "type": "Entire apartment",
+                    "beds": 3,
+                    "photo": "https://images.unsplash.com/photo-1494203484021-3c454daf695d?ixlib=rb-1.2.1&auto=format&fit=crop&w=2250&q=80"
+                },
+                {
+                    "city": "Helsinki",
+                    "country": "Finland",
+                    "superHost": false,
+                    "title": "Beautiful and comfortable old wooden house",
+                    "rating": 4.63,
+                    "maxGuests": 10,
+                    "type": "Entire house",
+                    "beds": null,
+                    "photo": "https://images.unsplash.com/photo-1516455590571-18256e5bb9ff?ixlib=rb-1.2.1&auto=format&fit=crop&w=2250&q=80"
+                },
+                {
+                    "city": "Turku",
+                    "country": "Finland",
+                    "superHost": false,
+                    "title": "Turku Nordic Home near city center",
+                    "rating": 4.24,
+                    "maxGuests": 5,
+                    "type": "Entire apartment",
+                    "beds": 3,
+                    "photo": "https://images.unsplash.com/photo-1519643381401-22c77e60520e?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjE3MzYxfQ&auto=format&fit=crop&w=2253&q=80"
+                },
+                {
+                    "city": "Turku",
+                    "country": "Finland",
+                    "superHost": true,
+                    "title": "Nice 2 room apartment close to everything",
+                    "rating": 4.34,
+                    "maxGuests": 6,
+                    "type": "Entire apartment",
+                    "beds": 3,
+                    "photo": "https://images.unsplash.com/photo-1523755231516-e43fd2e8dca5?ixlib=rb-1.2.1&auto=format&fit=crop&w=1275&q=80"
+                }
+            ]
+        }
+    },
+    components: {}
 }
 </script>
 
 <style lang="scss">
 #app {
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
+    -webkit-font-smoothing: antialiased;
+    -moz-osx-font-smoothing: grayscale;
 }
 </style>
--- tailwind.js
+++ tailwind.js
@@ -1,7 +1,36 @@
 module.exports = {
   purge: [],
   theme: {
-    extend: {},
+    extend: {
+      fontFamily : {
+        'montserrat' : ['Montserrat','sans-serif'],
+        'mulish' : ['Mulish','sans-serif'],
+        'poppins' : ['Poppins','sans-serif'],
+      },
+      fontSize : {
+        '10px' : '10px',
+      },
+      boxShadow : {
+        'white' : '0px 1px 6px rgba(0, 0, 0, 0.1)',
+      },
+      borderRadius : {
+        '12px' : '12px',
+        '16px' : '16px',
+        '24px' : '24px',
+      },
+      colors : {
+        'primary' : '#EB5757',
+        'primary-opaque' : 'rgba(235, 87, 87, 0.72)',
+        'secondary' : '#4F4F4F',
+        'tertiary' : '#828282',
+        'gray-1' : '#BDBDBD',
+        'off-white' : '#F2F2F2',
+      },
+      spacing : {
+        '33' : '9rem',
+        '90' : '90%',
+      }
+    },
   },
   variants: {},
   plugins: [],
